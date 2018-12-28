bb2962612e2a33e611edf63eeb85145f952504ec
Defelo <elodef42@gmail.com>
2018-12-29 00:29:19 +0100

    Add sigmoid prime

--- util.py
+++ util.py
@@ -6,5 +6,9 @@ def sigmoid(x: float) -> float:
     return 1 / (1 + math.exp(-x))
 
 
+def sigmoid_prime(x: float) -> float:
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
 def random():
     return _random.random() * 2 - 1
