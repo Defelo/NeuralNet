6a8a10a586aa4751dd7b09e37570dbe6553897c6
Defelo <elodef42@gmail.com>
2019-01-06 17:54:08 +0100

    Remove type hints for TrainingData

--- trainingdata.py
+++ trainingdata.py
@@ -1,7 +1,4 @@
-from matrix import Matrix
-
-
 class TrainingData:
-    def __init__(self, input_data: Matrix, expected_output: Matrix):
+    def __init__(self, input_data, expected_output):
         self.input_data = input_data
         self.expected_output = expected_output
